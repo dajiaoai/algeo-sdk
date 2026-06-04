5926699ed5a2ecdbef92fd93747ffd17827b2023
liluhui <liluhui@meideng.net>
2026-06-04 15:09:36 +0800

    feat: update version to 2.7.0 in README, package.json, package-lock.json, and SDK files

--- dist/algeo-sdk.umd.js
+++ dist/algeo-sdk.umd.js
@@ -3982,7 +3982,7 @@
   }
 
   /** SDK 版本号，构建时由 rollup 注入 */
-  const VERSION = '2.6.1';
+  const VERSION = '2.7.0';
   const DEFAULT_EMBED_BASE = 'https://dajiaoai.com';
   const DEFAULT_PRESENTATION_PATH = '/e';
   const DEFAULT_EDITOR_PATH = '/embed/edit';
